64b97feaa3fadd41363e71a9fc09aaaec5dc711a
Cedric Angulo <cdrcangulo@gmail.com>
2024-04-14 22:48:27 +0800

    Update nav.js

--- assets/js/components/nav.js
+++ assets/js/components/nav.js
@@ -111,7 +111,7 @@ const md_nav = `
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg> About </a>
-        <a class="md-nav-link hyperlink" href="contact" data-location="">
+        <a class="md-nav-link hyperlink" href="" data-location="contact">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
             </svg>
