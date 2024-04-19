60c2542034e6cd03bd98b70bc1f2607fcceae458
Cedric Angulo <cdrcangulo@gmail.com>
2024-04-19 23:59:35 +0800

    Add files via upload

--- assets/js/components/footer.js
+++ assets/js/components/footer.js
@@ -1,12 +1,29 @@
+const support_card = `
+<div class="support-container">
+    <div class="text-content">
+        <h1> Support Us </h1>
+        <p class="paragraph"> Help us keep WebWise free for all. Every donation fuels our mission to provide accessible web
+            development education. Join us in empowering learners worldwise! </p>
+    </div>
+    <div class="support-actions">
+        <a class="secondary-btn hyperlink" href="" data-location="">Learn more</a>
+        <a class="primary-btn hyperlink" href="" data-location=""> Donate now <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+            </svg>
+        </a>
+    </div>
+</div>
+`;
+
+const support = $('.support-wrapper').innerHTML = support_card;
+
 const footer_component = `
 <div class="footer-wrapper">
     <div class="footer-links-wrapper">
         <div class="footer-logo-container">
             <a href="" class="footer-logo-link hyperlink" data-location="home" title="Home of WebWise">
-                <!-- <img src="/assets/images/lugu.png" alt="Logo" /> -->
-                <svg id="svg-logo" xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" shape-rendering="geometricPrecision"
-                    text-rendering="geometricPrecision">
+                <svg id="svg-logo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
                     <g id="left" transform="translate(.000001 0.000001)">
                         <rect width="17" height="115" rx="8" ry="8"
                             transform="matrix(1.066595 0.803676-.890585 1.181937 102.417258 15.2588)" stroke-width="0" />
@@ -51,13 +68,13 @@ const footer_component = `
                 <h2 class="footer-grid-headers">Support</h2>
                 <ul class="footer-grid-list">
                     <li>
-                        <a href="">FAQ</a>
+                        <a class="hyperlink" href="" data-location="">FAQ</a>
                     </li>
                     <li>
-                        <a class="hyperlink" data-location="contact" href="">Contact</a>
+                        <a class="hyperlink" href="" data-location="contact">Contact</a>
                     </li>
                     <li>
-                        <a href="">Donate</a>
+                        <a class="hyperlink" href="" data-location="">Donate</a>
                     </li>
                 </ul>
             </div>
@@ -65,10 +82,10 @@ const footer_component = `
                 <h2 class="footer-grid-headers">Legal</h2>
                 <ul class="footer-grid-list">
                     <li>
-                        <a href="">Terms</a>
+                        <a class="hyperlink" href="" data-location="">Terms</a>
                     </li>
                     <li>
-                        <a href="">Privacy</a>
+                        <a class="hyperlink" href="" data-location="">Privacy</a>
                     </li>
                 </ul>
             </div>
@@ -90,23 +107,9 @@ const footer_component = `
                 </svg>
             </a>
             <a href="" target="_blank">
-                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
-                    <path fill-rule="evenodd"
-                        d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
-                        clip-rule="evenodd" />
-                </svg>
-            </a>
-            <a href="" target="_blank">
                 <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path fill-rule="evenodd"
                         d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                        clip-rule="evenodd" />
-                </svg>
-            </a>
-            <a href="" target="_blank">
-                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd"
-                        d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
                         clip-rule="evenodd" />
                 </svg>
             </a>
