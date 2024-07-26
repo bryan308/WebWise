029f1be0193bee22c95dd6439b3ed563175f3464
bryan308 <cdrcangulo@gmail.com>
2024-07-27 02:32:31 +0800

    Home done

---
+++ src/components/home/testimonials.ts
@@ -0,0 +1,34 @@
+export const testimonials = [
+	{
+		header: 'Simple yet Powerful!',
+		qoute:
+			'WebWise made learning web development so easy for me. The lessons were clear and straightforward. I’d recommend it to anyone looking to learn coding.',
+		src: 'avatar5',
+		name: 'Leanrey Fuenticilla',
+		school: 'Student at GORDON College',
+	},
+	{
+		header: 'User-Friendly Platform',
+		qoute:
+			'I found WebWise to be really user-friendly. The courses were easy to navigate, and I didn’t feel overwhelmed at all.',
+		src: 'avatar3',
+		name: 'Charles Benedict Liodones',
+		school: 'Student at NEUST',
+	},
+	{
+		header: 'Boosted My Confidence',
+		qoute:
+			'Thanks to WebWise, I feel more confident in my coding skills now. The courses were easy to understand, and I could learn at my own pace.',
+		src: 'avatar4',
+		name: 'Chollo Dela Cruz',
+		school: 'Student at NEUST',
+	},
+	{
+		header: 'Time-Saving Learning',
+		qoute:
+			'WebWise helped me save a lot of time. The lessons were straightforward, and I could learn whenever I had free time.',
+		src: 'avatar5',
+		name: 'Clent John Caseria',
+		school: 'Student at PRMSU',
+	},
+];
